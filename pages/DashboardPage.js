8ed491a6a9066f25974bd0a4dd84c490a146e42d
Onauma S <o5na.puchii@gmail.com>
2024-08-16 18:28:22 -0700

    Feature Admin main modules are updated and completed

--- pages/DashboardPage.js
+++ pages/DashboardPage.js
@@ -13,12 +13,13 @@ export class DashboardPage extends BasePage {
 
     this.bookLink = page.locator("//a[@href='#books' and @class='nav-link']");
     this.borrowingBooksLink = page.locator("//a[@class='nav-link' and contains(., 'Borrowing Books')]");
-    this.dashboardLink = page.locator("//span[@class='title' and text()='Dashboard']")
-    this.usersLink = page.locator("//span[@class='title' and text()='Users']");
+    this.dashboardLink = page.locator("//a[@class='nav-link' and contains(., 'Dashboard')]")
+    this.usersLink = page.locator("//a[@class='nav-link' and contains(., 'Users')]");
 
     this.bookTittle = page.locator("//span[@class='title' and text()='Books']");
     this.borrowingBookTittle = page.locator("//span[@class='title' and text()='Borrowing Books']");
     this.dashboardTittle = page.locator("//span[@class='title' and text()='Dashboard']")
+    this.userTittle = page.locator("//span[@class='title' and text()='Users']");
     
   }
 
